4288035e229f155d90c693439ec534a78e5a9151
Shrusti <Shrustimalviya98@gmail.com>
2023-10-01 23:45:46 +0530

    Add Readme

---
+++ js/script.js
@@ -0,0 +1,126 @@
+var swiper = new Swiper(".popular-content", {
+  spaceBetween: 30,
+  slidesPerView:1,
+
+  autoplay: {
+    delay: 77750,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    280:{
+        slidesPerView:1,
+        spaceBetween:10,
+    },
+
+    320:{
+        slidesPerView:2,
+        spaceBetween:10,
+    },
+
+    510:{
+        slidesPerView:2,
+        spaceBetween:10,
+    },
+
+    750:{
+        slidesPerView:3,
+        spaceBetween:15,
+    },
+
+    900:{
+        slidesPerView:4,
+        spaceBetween:20,
+    },
+  },
+});
+
+let playButton=document.querySelector('.play-movie');
+let video=document.querySelector('.video-container');
+let myvideo=document.querySelector('#myvideo');
+let closebtn=document.querySelector('.video-close');
+
+
+playButton.onclick = () => {
+  video.classList.add('show-video');
+  myvideo.play();
+};
+
+closebtn.onclick = () => {
+  video.classList.remove('show-video');
+  myvideo.pause();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const swiper = new Swiper('.popular-content', {
+
+//     slidesPerView:1,
+//     spaceBetween: 10,
+//     autoplay : {
+//       delay: 5000,
+//       disableOnInteraction:false,
+//     },
+
+//    pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   breakpoints:{
+//     280:{
+//         slidesPerView:1,
+//         spaceBetween:10,
+//     },
+
+//     320:{
+//         slidesPerView:2,
+//         spaceBetween:10,
+//     },
+
+//     510:{
+//         slidesPerView:2,
+//         spaceBetween:10,
+//     },
+
+//     750:{
+//         slidesPerView:3,
+//         spaceBetween:15,
+//     },
+
+//     900:{
+//         slidesPerView:4,
+//         spaceBetween:20,
+//     },
+//   },
+// });
